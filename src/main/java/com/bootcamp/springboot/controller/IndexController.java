c2ce6663abec854edec3874f02d5bf9b91f1885e
Kenny <kennymarlo.private@gmail.com>
2021-02-11 11:11:16 +0800

    add save employee function

--- src/main/java/com/bootcamp/springboot/controller/IndexController.java
+++ src/main/java/com/bootcamp/springboot/controller/IndexController.java
@@ -1,16 +1,23 @@
 package com.bootcamp.springboot.controller;
 
+import com.bootcamp.springboot.model.Employee;
 import com.bootcamp.springboot.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping({"/", "/index"})
 public class IndexController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private EmployeeService employeeService;
 
@@ -20,12 +27,17 @@ public class IndexController {
     }
 
     @GetMapping
-    public String main(Model model) {
+    public String main(Employee employee) {
         return "index";
     }
 
     @PostMapping
-    public String save(Model model) {
+    public String save(Employee employee) {
+        if (employee != null) {
+            logger.info("{}",employee);
+            employeeService.save(employee);
+        }
         return "saved";
     }
+
 }
